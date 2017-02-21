2b42262e18f4e2b0a7041c567baaee444dfbe6f1
Gerics11 <feher.gergo9@gmail.com>
2017-02-21 13:41:44 +0100

    ui improvements

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
         onAttack= "B";
         switchAttack();
         findViewById(R.id.winner_a).setVisibility(View.GONE);
