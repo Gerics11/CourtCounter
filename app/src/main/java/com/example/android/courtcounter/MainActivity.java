5880f637bdcfd0b6111fc437baa29b877c5e97e7
Gerics11 <feher.gergo9@gmail.com>
2017-03-27 17:55:30 +0200

    test

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.courtcounter;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         onAttack = "B";
         switchAttack();
-        findViewById(R.id.winner_a).setVisibility(View.GONE);
-        findViewById(R.id.winner_b).setVisibility(View.GONE);
         if (savedInstanceState != null) {
             scoreTeamA = savedInstanceState.getInt(TEAMASCORE);
             scoreTeamB = savedInstanceState.getInt(TEAMBSCORE);
@@ -50,113 +47,58 @@ public class MainActivity extends AppCompatActivity {
         outState.putString(ATTACKER, onAttack);
     }
 
-    /**
-     * Displays the given score for Team A.
-     *
-     * @param number takes scores from methods.
-     */
     public void display(int number) {
         TextView countViewer = (TextView) findViewById(R.id.team_a_score);
-        countViewer.setText("" + number);
+        countViewer.setText(String.valueOf(number));
     }
-
-    /**
-     * Displays the given score for Team B.
-     *
-     * @param number takes scores from methods.
-     */
     public void display2(int number) {
         TextView countViewer = (TextView) findViewById(R.id.team_b_score);
-        countViewer.setText("" + number);
+        countViewer.setText(String.valueOf(number));
     }
 
-    /**
-     * Adds one to Team A score.
-     *
-     * @param view updates team score
-     */
+
     public void scoreOneTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         display(scoreTeamA);
         winCheckTeamA();
     }
-
-    /**
-     * Adds two to Team A score
-     *
-     * @param view updates team score
-     */
     public void scoreTwoTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         display(scoreTeamA);
         winCheckTeamA();
     }
-
-    /**
-     * Adds one to Team B score
-     *
-     * @param view updates team score
-     */
     public void scoreOneTeamB(View view) {
         scoreTeamB = scoreTeamB + 1;
-        winCheckTeamB();
         display2(scoreTeamB);
+        winCheckTeamB();
     }
-
-    /**
-     * Adds two to Team B score
-     *
-     * @param view updates team score
-     */
     public void scoreTwoTeamB(View view) {
         scoreTeamB = scoreTeamB + 2;
-        winCheckTeamB();
         display2(scoreTeamB);
+        winCheckTeamB();
     }
 
-    /**
-     * Switches attacking side via switchattack method.
-     *
-     * @param view Activated by Rebound buttons.
-     */
+
     public void rebound(View view) {
         switchAttack();
     }
 
-    /**
-     * Method run by Team A score buttons.
-     * If there is a winner, buttons except reset are disabled, and WINNER textview
-     * replaces the teams score.
-     */
     public void winCheckTeamA() {
         if (scoreTeamA >= 21) {
-            findViewById(R.id.winner_a).setVisibility(View.VISIBLE);
-            findViewById(R.id.team_a_score).setVisibility(View.GONE);
-            disable_A();
-            disable_B();
-        } else {
+            TextView TV = (TextView)findViewById(R.id.team_a_score);
+            TV.setText(getString(R.string.winner));
+            scoreButtonState("locked");
         }
     }
 
-    /**
-     * Method run by Team A score buttons.
-     * If there is a winner, buttons except reset are disabled, and WINNER textview
-     * replaces the teams score.
-     */
     public void winCheckTeamB() {
         if (scoreTeamB >= 21) {
-            findViewById(R.id.winner_b).setVisibility(View.VISIBLE);
-            findViewById(R.id.team_b_score).setVisibility(View.GONE);
-            disable_A();
-            disable_B();
-        } else {
+            TextView TV = (TextView)findViewById(R.id.team_b_score);
+            TV.setText(getString(R.string.winner));
+            scoreButtonState("locked");
         }
     }
 
-    /**
-     * *Reset Button: resets both scores to 0, reset attacking side to Team A,
-     * resets winner textview to team score.
-     */
     public void resetScore(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
@@ -164,80 +106,53 @@ public class MainActivity extends AppCompatActivity {
         display2(scoreTeamB);
         onAttack = "B";
         switchAttack();
-        findViewById(R.id.winner_a).setVisibility(View.GONE);
-        findViewById(R.id.winner_b).setVisibility(View.GONE);
-        findViewById(R.id.team_a_score).setVisibility(View.VISIBLE);
-        findViewById(R.id.team_b_score).setVisibility(View.VISIBLE);
-        enable_A();
+        scoreButtonState("attackA");
     }
 
-    /**
-     * Swtiches the attacking team, gives gray backgroud to attacking team, and
-     * disables defensive teams buttons.
-     */
     public void switchAttack() {
         LinearLayout layoutA = (LinearLayout) findViewById(R.id.team_a_layout);
         LinearLayout layoutB = (LinearLayout) findViewById(R.id.team_b_layout);
         if (onAttack.equals("A")) {
             onAttack = "B";
-            layoutB.setBackgroundColor(Color.parseColor("#A4A4A4"));
-            layoutA.setBackgroundColor(0x00000000);
-            disable_A();
-            enable_B();
+            layoutB.setBackgroundResource(R.color.light_gray);
+            layoutA.setBackgroundResource(R.color.transparent);
+            scoreButtonState("attackB");
         } else {
             onAttack = "A";
-            layoutA.setBackgroundColor(Color.parseColor("#A4A4A4"));
-            layoutB.setBackgroundColor(0x00000000);
-            enable_A();
-            disable_B();
+            layoutA.setBackgroundResource(R.color.light_gray);
+            layoutB.setBackgroundResource(R.color.transparent);
+            scoreButtonState("attackA");
         }
     }
 
-    /**
-     * Disable Team A related buttons. Used by methods for switching attacking side and reset.
-     */
-    public void disable_A() {
+    private void scoreButtonState(String buttonState){
         Button a_3pt = (Button) findViewById(R.id.team_a_2pt);
         Button a_2pt = (Button) findViewById(R.id.team_a_1pt);
         Button a_rb = (Button) findViewById(R.id.team_a_rb);
-        a_3pt.setEnabled(false);
-        a_2pt.setEnabled(false);
-        a_rb.setEnabled(false);
-    }
-
-    /**
-     * Disable Team B related buttons. Used by methods for switching attacking side and reset.
-     */
-    public void disable_B() {
         Button b_3pt = (Button) findViewById(R.id.team_b_2pt);
         Button b_2pt = (Button) findViewById(R.id.team_b_1pt);
         Button b_rb = (Button) findViewById(R.id.team_b_rb);
-        b_3pt.setEnabled(false);
-        b_2pt.setEnabled(false);
-        b_rb.setEnabled(false);
-    }
-
-    /**
-     * Enable Team A related buttons. Used by methods for switching attacking side and reset.
-     */
-    public void enable_A() {
-        Button a_3pt = (Button) findViewById(R.id.team_a_2pt);
-        Button a_2pt = (Button) findViewById(R.id.team_a_1pt);
-        Button a_rb = (Button) findViewById(R.id.team_a_rb);
-        a_3pt.setEnabled(true);
-        a_2pt.setEnabled(true);
-        a_rb.setEnabled(true);
-    }
-
-    /**
-     * Enable Team B related buttons. Used by methods for switching attacking side and reset.
-     */
-    public void enable_B() {
-        Button b_3pt = (Button) findViewById(R.id.team_b_2pt);
-        Button b_2pt = (Button) findViewById(R.id.team_b_1pt);
-        Button b_rb = (Button) findViewById(R.id.team_b_rb);
-        b_3pt.setEnabled(true);
-        b_2pt.setEnabled(true);
-        b_rb.setEnabled(true);
+        switch (buttonState) {
+            case "attackA": a_3pt.setEnabled(true);
+                            a_2pt.setEnabled(true);
+                            a_rb.setEnabled(true);
+                            b_3pt.setEnabled(false);
+                            b_2pt.setEnabled(false);
+                            b_rb.setEnabled(false);
+                            break;
+            case "attackB": a_3pt.setEnabled(false);
+                            a_2pt.setEnabled(false);
+                            a_rb.setEnabled(false);
+                            b_3pt.setEnabled(true);
+                            b_2pt.setEnabled(true);
+                            b_rb.setEnabled(true);
+                            break;
+            case "locked":  a_3pt.setEnabled(false);
+                            a_2pt.setEnabled(false);
+                            a_rb.setEnabled(false);
+                            b_3pt.setEnabled(false);
+                            b_2pt.setEnabled(false);
+                            b_rb.setEnabled(false);
+        }
     }
 }
